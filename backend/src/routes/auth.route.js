e014a81fdf734a703d557d89789ba6ef10724261
codewithayush7 <codewithayush7@gmail.com>
2025-06-06 23:31:56 +0530

    after copying

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import { checkAuth, signup, login, logout, updateProfile } from '../controllers/auth.controller.js';
-import { protectRoute } from '../middleware/auth.middleware.js';
+import express from "express";
+import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
-const router=express.Router();
+const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
